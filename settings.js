e8bc844b576c852ff4f4fa88e45f5069b87bb4d7
Anuwa666 <anuwanew543@gmail.com>
2025-04-05 23:26:48 +0530

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+
+SESSION_ID: process.env.SESSION_ID || "session id",
+PREFIX: process.env.PREFIX || '.' ,
+POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://postgres:lakamd@db.ndduyrkebubwlbwiljee.supabase.co:5432/postgres' : process.env.POSTGRESQL_URL
+};
